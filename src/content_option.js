88eb76c113693c54ff1e9c1fa659574b0b7a7236
om aher <omaher2605@gmail.com>
2024-02-23 13:19:30 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,107 @@
+const logotext = "om";
+const meta = {
+  title: "om Aher",
+  description:
+    "I’m Om Aher data scientist _ Full stack devloper,currently working in Sambhaji Nagar",
+};
+
+const introdata = {
+  title: "I’m Om Aher",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description: "i love to code",
+  your_img_url:
+    "https://e0.pxfuel.com/wallpapers/974/834/desktop-wallpaper-programming-is-easy-coder-computer-code-blackandwhite-desk-programmer-black-laptop.jpg",
+};
+
+const dataabout = {
+  title: "About my self",
+  aboutme:
+    "Hey there! 👋 I'm OM, a passionate full-stack web developer with a love for crafting digital experiences that leave a lasting impact.",
+};
+
+const skills = [
+  {
+    name: "JAVA",
+    value: 90,
+  },
+
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "My-SQL",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description: "i love to code in HTML,CSS,JavaSript & React.",
+  },
+  {
+    title: "Mobile Apps",
+    description: "i love to code in Android & Flutter.",
+  },
+  {
+    title: "DataBases",
+    description: "i love to work with My-SQL .",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://images.unsplash.com/photo-1607252650355-f7fd0460ccdb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5kcm9pZCUyMGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D",
+    description: "I develop an android app project Named - OP Browser.",
+    link: "#",
+  },
+  {
+    img: "https://themeforest.img.customer.envatousercontent.com/files/190348892/Theme+Preview/03_Preview.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=0e54a0b27459b7af0809d6629899d064",
+    description: "Project of creating sport e-commerce website",
+    link: "#",
+  },
+  {
+    img: "https://www.researchgate.net/publication/357629290/figure/fig3/AS:1114627895242754@1642759407849/IOT-based-smart-agriculture-monitoring-system.png",
+    description: "Create an IOT Project to automate Agricultural System",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "omaher2605@gmail.com",
+  YOUR_FONE: "8975383040",
+  description: "You can contact me ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/dashboard",
+  facebook: "https://instagram.com/omaher_47?igshid=OGQ5ZDc2ODk2ZA==",
+  linkedin:
+    "https://www.linkedin.com/in/om-aher-965a00279?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
